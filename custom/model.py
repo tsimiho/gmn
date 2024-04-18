5a35ae7bd53ea08bdfbdbf57a84d944292d29534
Achilleas Tsimichodimos <achillestsimiho@gmail.com>
2024-04-18 15:11:40 +0300

    testing acc again

---
+++ custom/model.py
@@ -0,0 +1,133 @@
+import torch
+from torch.nn import Linear
+from torch.nn import functional as F
+from torch_geometric.data import Data
+from torch_geometric.nn import TopKPooling
+
+from layers import GraphAggregator, GraphMatchingConvolution
+from utils import create_batch
+
+
+class GraphMatchingNetwork(torch.nn.Module):
+    def __init__(self, args):
+        super(GraphMatchingNetwork, self).__init__()
+        self.args = args
+        self.margin = self.args.margin
+        if args.n_classes > 2:
+            self.f1_average = "micro"
+        else:
+            self.f1_average = "binary"
+        self.layers = torch.nn.ModuleList()
+        self.layers.append(
+            GraphMatchingConvolution(self.args.feat_dim, self.args.dim, args)
+        )
+        for _ in range(self.args.num_layers - 1):
+            self.layers.append(
+                GraphMatchingConvolution(self.args.dim, self.args.dim, args)
+            )
+        self.aggregator = GraphAggregator(self.args.dim, self.args.dim, self.args)
+        self.layer_outputs = []
+        self.layer_cross_attentions = []
+        self.mincut = []
+        self.mlp = torch.nn.Sequential()
+        self.args.n_clusters = args.n_clusters
+        self.mlp.append(Linear(self.args.dim, self.args.n_clusters))
+        self.topk_pooling = TopKPooling(self.args.dim, ratio=8)
+        self.topk_outputs = []
+
+    def compute_emb(
+        self, feats, edge_index, batch, sizes_1, sizes_2, edge_index_1, edge_index_2
+    ):
+        for i in range(self.args.num_layers):
+            (
+                feats,
+                edge_index,
+                batch,
+                (attention_input, cross_graph_attention, a_x, a_y),
+            ) = self.layers[i](feats, edge_index, batch)
+            x_1 = feats[: sizes_1.item(), :]
+            x_2 = feats[sizes_1.item() : sizes_1.item() + sizes_2.item(), :]
+            x_pooled_1, edge_index_pooled_1, _, _, perm1, _ = self.topk_pooling(
+                x_1,
+                edge_index_1,
+            )
+            x_pooled_2, edge_index_pooled_2, _, _, perm2, _ = self.topk_pooling(
+                x_2,
+                edge_index_2,
+            )
+            self.topk_outputs.append(
+                (
+                    (x_pooled_1, edge_index_pooled_1, perm1),
+                    (x_pooled_2, edge_index_pooled_2, perm2),
+                )
+            )
+            self.layer_cross_attentions.append((cross_graph_attention, a_x, a_y))
+            self.layer_outputs.append((x_1, x_2))
+
+        feats = self.aggregator(feats, edge_index, batch)
+        return feats, edge_index, batch
+
+    def combine_pair_embedding(
+        self, feats_1, edge_index_1, feats_2, edge_index_2, sizes_1, sizes_2
+    ):
+        feats = torch.cat([feats_1, feats_2], dim=0)
+        max_node_idx_1 = sizes_1.sum()
+        edge_index_2_offset = edge_index_2 + max_node_idx_1
+        edge_index = torch.cat([edge_index_1, edge_index_2_offset], dim=1)
+        batch = create_batch(torch.cat([sizes_1, sizes_2], dim=0))
+        feats, edge_index, batch = (
+            feats.to(self.args.device),
+            edge_index.to(self.args.device),
+            batch.to(self.args.device),
+        )
+        return feats, edge_index, batch
+
+    def forward(self, feats_1, edge_index_1, feats_2, edge_index_2, sizes_1, sizes_2):
+        self.layer_outputs = []
+        self.layer_cross_attentions = []
+        self.topk_outputs = []
+        self.mincut = []
+        feats, edge_index, batch = self.combine_pair_embedding(
+            feats_1, edge_index_1, feats_2, edge_index_2, sizes_1, sizes_2
+        )
+        emb, _, _ = self.compute_emb(
+            feats, edge_index, batch, sizes_1, sizes_2, edge_index_1, edge_index_2
+        )
+        emb_1 = emb[: emb.shape[0] // 2, :]
+        emb_2 = emb[emb.shape[0] // 2 :, :]
+
+        best_acc1, best_acc2 = 0.0, 0.0
+        cluster1, cluster2 = None, None
+        for i in range(len(self.topk_outputs)):
+            (
+                (x_pooled_1, edge_index_pooled_1, perm1),
+                (x_pooled_2, edge_index_pooled_2, perm2),
+            ) = self.topk_outputs[i]
+            acc1 = len(set(range(8)) & set(perm1.tolist()))
+            acc2 = len(set(range(8)) & set(perm2.tolist()))
+            if acc1 > best_acc1:
+                cluster1 = Data(x=x_pooled_1, edge_index=edge_index_pooled_1)
+                best_acc1 = acc1
+            if acc2 > best_acc2:
+                cluster2 = Data(x=x_pooled_2, edge_index=edge_index_pooled_2)
+                best_acc2 = acc2
+
+        return emb_1, emb_2, cluster1, cluster2
+
+    def compute_metrics(self, emb_1, emb_2, labels):
+        distances = torch.norm(emb_1 - emb_2, p=2, dim=1)
+        loss = F.relu(self.margin - labels * (1 - distances)).mean()
+        predicted_similar = torch.where(
+            distances < self.args.margin,
+            torch.ones_like(labels),
+            -torch.ones_like(labels),
+        )
+        acc = (predicted_similar == labels).float().mean()
+        metrics = {"loss": loss, "acc": acc}
+        return metrics
+
+    def init_metric_dict(self):
+        return {"acc": -1, "f1": -1}
+
+    def has_improved(self, m1, m2):
+        return m1["acc"] < m2["acc"]
