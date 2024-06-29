159fb72a2ace23348721871c67bd93043c9fd59b
Achilleas Tsimichodimos <achillestsimiho@gmail.com>
2024-06-29 14:20:07 +0300

    over 60

---
+++ gmn.py
@@ -0,0 +1,225 @@
+import torch
+import torch.nn.functional as F
+from torch.nn import Linear
+from torch.nn import functional as F
+from torch_geometric.data import Data
+from torch_geometric.nn import BatchNorm, MessagePassing, TopKPooling
+from torch_scatter import scatter_mean
+
+from custom.utils import *
+
+
+class GraphMatchingConvolution(MessagePassing):
+    def __init__(self, in_channels, out_channels, args, aggr="add"):
+        super(GraphMatchingConvolution, self).__init__(aggr=aggr)
+        self.args = args
+        self.lin_node = torch.nn.Linear(in_channels, out_channels)
+        self.lin_message = torch.nn.Linear(out_channels * 2, out_channels)
+        self.lin_passing = torch.nn.Linear(out_channels + in_channels, out_channels)
+        self.batch_norm = BatchNorm(out_channels)
+
+    def forward(self, x, edge_index, batch):
+        x_transformed = self.lin_node(x)
+        return self.propagate(edge_index, x=x_transformed, original_x=x, batch=batch)
+
+    def message(self, edge_index_i, x_i, x_j):
+        x = torch.cat([x_i, x_j], dim=1)
+        m = self.lin_message(x)
+        return m
+
+    def update(self, aggr_out, edge_index, x, original_x, batch):
+        n_graphs = torch.unique(batch).shape[0]
+        cross_graph_attention, a_x, a_y = batch_block_pair_attention(
+            original_x, batch, n_graphs
+        )
+        attention_input = original_x - cross_graph_attention
+        aggr_out = self.lin_passing(torch.cat([aggr_out, attention_input], dim=1))
+        aggr_out = self.batch_norm(aggr_out)
+
+        norms = torch.norm(aggr_out, p=2, dim=1)
+        cross_attention_sums = cross_graph_attention.sum(dim=1)
+
+        return (
+            aggr_out,
+            edge_index,
+            batch,
+            (
+                attention_input,
+                cross_graph_attention,
+                a_x,
+                a_y,
+                norms,
+                cross_attention_sums,
+            ),
+        )
+
+
+class GraphAggregator(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, args):
+        super(GraphAggregator, self).__init__()
+        self.lin = torch.nn.Linear(in_channels, out_channels)
+        self.lin_gate = torch.nn.Linear(in_channels, out_channels)
+        self.lin_final = torch.nn.Linear(out_channels, out_channels)
+        self.args = args
+
+    def forward(self, x, edge_index, batch):
+        x_states = self.lin(x)
+        x_gates = torch.nn.functional.softmax(self.lin_gate(x), dim=1)
+        x_states = x_states * x_gates
+        x_states = scatter_mean(x_states, batch, dim=0)
+        x_states = self.lin_final(x_states)
+        return x_states
+
+
+class GraphMatchingNetwork(torch.nn.Module):
+    def __init__(self, args):
+        super(GraphMatchingNetwork, self).__init__()
+        self.args = args
+        self.margin = self.args.margin
+        if args.n_classes > 2:
+            self.f1_average = "micro"
+        else:
+            self.f1_average = "binary"
+        self.layers = torch.nn.ModuleList()
+        self.layers.append(
+            GraphMatchingConvolution(self.args.feat_dim, self.args.dim, args)
+        )
+        for _ in range(self.args.num_layers - 1):
+            self.layers.append(
+                GraphMatchingConvolution(self.args.dim, self.args.dim, args)
+            )
+        self.aggregator = GraphAggregator(self.args.dim, self.args.dim, self.args)
+        self.layer_outputs = []
+        self.layer_cross_attentions = []
+        self.mincut = []
+        self.mlp = torch.nn.Sequential()
+        self.args.n_clusters = args.n_clusters
+        self.mlp.append(Linear(self.args.dim, self.args.n_clusters))
+        self.topk_outputs = []
+        self.norms_per_layer = []
+        self.attention_sums_per_layer = []
+        self.topk_pooling = TopKPooling(self.args.dim, ratio=0.99)
+
+    def compute_emb(
+        self, feats, edge_index, batch, sizes_1, sizes_2, edge_index_1, edge_index_2
+    ):
+
+        for i in range(self.args.num_layers):
+            (
+                feats,
+                edge_index,
+                batch,
+                (
+                    attention_input,
+                    cross_graph_attention,
+                    a_x,
+                    a_y,
+                    norms,
+                    attention_sums,
+                ),
+            ) = self.layers[i](feats, edge_index, batch)
+
+            x_1 = feats[: sizes_1.item(), :]
+            x_2 = feats[sizes_1.item() : sizes_1.item() + sizes_2.item(), :]
+
+            norms_1 = norms[: sizes_1.item()]
+            norms_2 = norms[sizes_1.item() : sizes_1.item() + sizes_2.item()]
+
+            attention_sums_1 = attention_sums[: sizes_1.item()]
+            attention_sums_2 = attention_sums[
+                sizes_1.item() : sizes_1.item() + sizes_2.item()
+            ]
+
+            x_pooled_1, edge_index_pooled_1, _, _, perm1, score1 = self.topk_pooling(
+                x_1,
+                edge_index_1,
+            )
+            x_pooled_2, edge_index_pooled_2, _, _, perm2, score2 = self.topk_pooling(
+                x_2,
+                edge_index_2,
+            )
+
+            self.topk_outputs.append(
+                (
+                    (x_pooled_1, edge_index_pooled_1, perm1, score1),
+                    (x_pooled_2, edge_index_pooled_2, perm2, score2),
+                )
+            )
+            self.layer_cross_attentions.append((cross_graph_attention, a_x, a_y))
+            self.layer_outputs.append((x_1, edge_index_1, x_2, edge_index_2))
+            self.norms_per_layer.append((norms_1, norms_2))
+            self.attention_sums_per_layer.append((attention_sums_1, attention_sums_2))
+
+        feats = self.aggregator(feats, edge_index, batch)
+        return feats, edge_index, batch
+
+    def combine_pair_embedding(
+        self, feats_1, edge_index_1, feats_2, edge_index_2, sizes_1, sizes_2
+    ):
+        feats = torch.cat([feats_1, feats_2], dim=0)
+        max_node_idx_1 = sizes_1.sum()
+        edge_index_2_offset = edge_index_2 + max_node_idx_1
+        edge_index = torch.cat([edge_index_1, edge_index_2_offset], dim=1)
+        batch = create_batch(torch.cat([sizes_1, sizes_2], dim=0))
+        feats, edge_index, batch = (
+            feats.to(self.args.device),
+            edge_index.to(self.args.device),
+            batch.to(self.args.device),
+        )
+        return feats, edge_index, batch
+
+    def forward(self, feats_1, edge_index_1, feats_2, edge_index_2, sizes_1, sizes_2):
+        self.layer_outputs = []
+        self.layer_cross_attentions = []
+        self.topk_outputs = []
+        self.mincut = []
+        self.norms_per_layer = []
+        self.attention_sums_per_layer = []
+
+        feats, edge_index, batch = self.combine_pair_embedding(
+            feats_1, edge_index_1, feats_2, edge_index_2, sizes_1, sizes_2
+        )
+        emb, _, _ = self.compute_emb(
+            feats, edge_index, batch, sizes_1, sizes_2, edge_index_1, edge_index_2
+        )
+        emb_1 = emb[: emb.shape[0] // 2, :]
+        emb_2 = emb[emb.shape[0] // 2 :, :]
+
+        best_acc1, best_acc2 = 0.0, 0.0
+        cluster1, cluster2 = None, None
+        layer1, layer2 = None, None
+        for i in range(len(self.topk_outputs)):
+            (
+                (x_pooled_1, edge_index_pooled_1, perm1, score1),
+                (x_pooled_2, edge_index_pooled_2, perm2, score2),
+            ) = self.topk_outputs[i]
+            acc1 = len(set(range(8)) & set(perm1.tolist()))
+            acc2 = len(set(range(8)) & set(perm2.tolist()))
+            if acc1 > best_acc1:
+                cluster1 = Data(x=x_pooled_1, edge_index=edge_index_pooled_1)
+                best_acc1 = acc1
+                layer1 = i + 1
+            if acc2 > best_acc2:
+                cluster2 = Data(x=x_pooled_2, edge_index=edge_index_pooled_2)
+                best_acc2 = acc2
+                layer2 = i + 1
+
+        return emb_1, emb_2, cluster1, cluster2, layer1, layer2
+
+    def compute_metrics(self, emb_1, emb_2, labels):
+        distances = torch.norm(emb_1 - emb_2, p=2, dim=1)
+        loss = F.relu(self.margin - labels * (1 - distances)).mean()
+        predicted_similar = torch.where(
+            distances < self.args.margin,
+            torch.ones_like(labels),
+            -torch.ones_like(labels),
+        )
+        acc = (predicted_similar == labels).float().mean()
+        metrics = {"loss": loss, "acc": acc}
+        return metrics
+
+    def init_metric_dict(self):
+        return {"acc": -1, "f1": -1}
+
+    def has_improved(self, m1, m2):
+        return m1["acc"] < m2["acc"]
--- mcs.py
+++ mcs.py
@@ -62,7 +62,6 @@ def print_patterns(patterns, graph1, graph2):
             )
 
 
-# if (not torch.equal(G1.x[n1], G2.x[n2])) or (not (n1, n2) in self.mp):
 
 
 def mutual_pairs(attention_nodes, i=0):
@@ -82,21 +81,19 @@ def mutual_pairs(attention_nodes, i=0):
     return mutual_pairs
 
 
-class VF2MCES:
-    def __init__(self, mp, time_limit=60):
+class MCS:
+    def __init__(self, mp):
         self.mp = mp
-        self.time_limit = time_limit
         self.start_time = None
         self.max_size = 0
         self.all_mappings = []
         self.unique_mappings = set()
 
-    def find_mces(self, G1, G2, max_depth=1000):
+    def find_mcs(self, G1, G2):
         self.max_size = 0
         self.all_mappings = []
         self.unique_mappings = set()
         self.start_time = time.time()
-        self.max_depth = max_depth
 
         G1_degrees = degree(G1.edge_index[0], G1.num_nodes)
         G2_degrees = degree(G2.edge_index[0], G2.num_nodes)
@@ -108,13 +105,14 @@ class VF2MCES:
             for n2 in nodes2:
                 if (n1, n2) in self.mp:
                     M = {n1: n2}
-                    self.match(G1, G2, M, G1_degrees, G2_degrees, 1)
+                    neighbors1 = set(G1.edge_index[1][G1.edge_index[0] == n1].tolist())
+                    neighbors2 = set(G2.edge_index[1][G2.edge_index[0] == n2].tolist())
+                    self.match(
+                        G1, G2, M, G1_degrees, G2_degrees, neighbors1, neighbors2
+                    )
         return self.all_mappings
 
-    def match(self, G1, G2, M, G1_degrees, G2_degrees, depth):
-        if time.time() - self.start_time > self.time_limit:
-            return
-
+    def match(self, G1, G2, M, G1_degrees, G2_degrees, neighbors1, neighbors2):
         if len(M) > self.max_size:
             self.max_size = len(M)
             self.all_mappings = [M.copy()]
@@ -125,28 +123,55 @@ class VF2MCES:
                 self.all_mappings.append(M.copy())
                 self.unique_mappings.add(canonical)
 
-        if depth >= self.max_depth:
-            return
+        candidates1 = sorted(neighbors1, key=lambda n: -G1_degrees[n].item())
+        candidates2 = sorted(neighbors2, key=lambda n: -G2_degrees[n].item())
 
-        nodes1 = sorted(range(G1.num_nodes), key=lambda n: -G1_degrees[n].item())
-        nodes2 = sorted(range(G2.num_nodes), key=lambda n: -G2_degrees[n].item())
-
-        for n1 in nodes1:
+        for n1 in candidates1:
             if n1 not in M:
-                for n2 in nodes2:
+                for n2 in candidates2:
                     if n2 not in M.values() and self.feasible(
                         n1, n2, M, G1, G2, G1_degrees, G2_degrees
                     ):
                         M[n1] = n2
-                        self.match(G1, G2, M, G1_degrees, G2_degrees, depth + 1)
+                        new_neighbors1 = set(
+                            G1.edge_index[1][G1.edge_index[0] == n1].tolist()
+                        )
+                        new_neighbors2 = set(
+                            G2.edge_index[1][G2.edge_index[0] == n2].tolist()
+                        )
+                        neighbors1.update(new_neighbors1 - set(M.keys()))
+                        neighbors2.update(new_neighbors2 - set(M.values()))
+                        self.match(
+                            G1,
+                            G2,
+                            M,
+                            G1_degrees,
+                            G2_degrees,
+                            neighbors1,
+                            neighbors2,
+                        )
                         del M[n1]
+                        neighbors1.difference_update(new_neighbors1)
+                        neighbors2.difference_update(new_neighbors2)
 
     def feasible(self, n1, n2, M, G1, G2, G1_degrees, G2_degrees):
         if not torch.equal(G1.x[n1], G2.x[n2]):
             return False
-        if G1_degrees[n1].item() > G2_degrees[n2].item():
-            return False
         if (n1, n2) not in self.mp:
+            return False
+
+        count1 = 0
+        count2 = 0
+
+        for neighbor in G1.edge_index[1][G1.edge_index[0] == n1]:
+            if neighbor.item() in M:
+                count1 += 1
+
+        for neighbor in G2.edge_index[1][G2.edge_index[0] == n2]:
+            if neighbor.item() in M.values():
+                count2 += 1
+
+        if count1 != count2:
             return False
 
         for neighbor in G1.edge_index[1][G1.edge_index[0] == n1]:
@@ -158,82 +183,9 @@ class VF2MCES:
             ):
                 return False
 
-        G1_edges = set(
-            (min(n1, n2), max(n1, n2))
-            for n1, n2 in zip(G1.edge_index[0].tolist(), G1.edge_index[1].tolist())
-        )
-        G2_edges = set(
-            (min(n1, n2), max(n1, n2))
-            for n1, n2 in zip(G2.edge_index[0].tolist(), G2.edge_index[1].tolist())
-        )
-        mapped_edges = set(
-            (min(M[n1], M[n2]), max(M[n1], M[n2]))
-            for n1, n2 in G1_edges
-            if n1 in M and n2 in M
-        )
-
-        if not all(edge in G2_edges for edge in mapped_edges):
-            return False
-
-        G1_in = {
-            neighbor.item()
-            for neighbor in G1.edge_index[1][G1.edge_index[0] == n1]
-            if neighbor.item() in M
-        }
-        G2_in = {M[neighbor] for neighbor in G1_in}
-        G1_out = {
-            neighbor.item()
-            for neighbor in G1.edge_index[1][G1.edge_index[0] == n1]
-            if neighbor.item() not in M
-        }
-        G2_out = {
-            neighbor.item()
-            for neighbor in G2.edge_index[1][G2.edge_index[0] == n2]
-            if neighbor.item() not in M.values()
-        }
-
-        if len(G1_in) > len(G2_in) or len(G1_out) > len(G2_out):
-            return False
-
-        G1_neighbors = {
-            neighbor.item() for neighbor in G1.edge_index[1][G1.edge_index[0] == n1]
-        }
-        G2_neighbors = {
-            neighbor.item() for neighbor in G2.edge_index[1][G2.edge_index[0] == n2]
-        }
-        for neighbor in G1_neighbors:
-            if neighbor in M and M[neighbor] not in G2_neighbors:
-                return False
-
-        if not self.is_connected_subgraph(M, G1) or not self.is_connected_subgraph(
-            M, G2, reverse=True
-        ):
-            return False
-
         return True
 
-    def is_connected_subgraph(self, M, G, reverse=False):
-        if reverse:
-            subgraph_nodes = set(M.values())
-            edge_index = G.edge_index
-        else:
-            subgraph_nodes = set(M.keys())
-            edge_index = G.edge_index
-
-        if len(subgraph_nodes) < 2:
-            return True
-
-        subgraph_edges = [
-            (u.item(), v.item())
-            for u, v in zip(edge_index[0], edge_index[1])
-            if u.item() in subgraph_nodes and v.item() in subgraph_nodes
-        ]
-
-        if not subgraph_edges:
-            return False
-
-        subgraph = nx.Graph(subgraph_edges)
-        return nx.is_connected(subgraph)
-
-    def canonical_form(self, mapping):
-        return tuple(sorted(mapping.items()))
+    def canonical_form(self, M):
+        G1_set = set(M.keys())
+        G2_set = set(M.values())
+        return (frozenset(G1_set), frozenset(G2_set))
